5a05a65d50a113d5cfb0f423908d25d18262b27b
Khushaldobariya <khushaldobariya.77@gmail.com>
2022-03-30 09:03:22 +0530

    30_1

--- src/App.js
+++ src/App.js
@@ -2,82 +2,83 @@
 import React from "react"; 
 
 function App(props) {
-  let  data = [
-    {
-      name: "amit",
-      age: 35,
-      salary: 40000,
-      bonus: 1000,
-      status: true
-    },
-    {
-      name: "ajay",
-      age: 25,
-      salary: 38000,
-      bonus: 2000,
-      status: false
-    },
-    {
-      name: "mayur",
-      age: 23,
-      salary: 50000,
-      bonus: 500,
-      status: true
-    },
-    {
-      name: "jay",
-      age: 29,
-      salary: 35000,
-      bonus: 600,
-      status: true
-    },
-    {
-      name: "raj",
-      age: 33,
-      salary: 22000,
-      bonus: 2000,
-      status: true
-    },
-  ]
-
-  let filterdata = data.filter ((d,index)=>(d.salary > 900 && d.status === true )); 
-  let total = filterdata.reduce ((acc,d) =>acc+d.salary+d.bonus, 0);
-
-
-  console.log(total);
-   return (
-     
-      <table border ="1" style={{textAlign:"center",margin:"20px"}}>
-        <h1 > Employlee data</h1>
+   let  Medicine_Data =
+  [
+     {
+       id: 101,
+       name: 'Abacavir',
+       quantity: 25,
+       price: 150,
+       expiry: 2022,
+       status: true
+     },
+     {
+       id: 102,
+       name: 'Eltrombopag',
+       quantity: 90,
+       price: 550,
+       expiry: 2021,
+       status: true
+     },
+     {
+       id: 103,
+       name: 'Meloxicam',
+       quantity: 85,
+       price: 450,
+       expiry: 2025,
+       status: false
+     },
+     {
+       id: 104,
+       name: 'Allopurinol',
+       quantity: 50,
+       price: 600,
+       expiry: 2023,
+       status: true
+     },
+     {
+       id: 105,
+       name: 'Phenytoin',
+       quantity: 63,
+       price: 250,
+       expiry: 2021,
+       status: false
+     }
+   ];
+   let filterdata = Medicine_Data.filter((d,i)=>( d.price > 200 && d.expiry > 2021))
+   let total = filterdata.reduce(( acc,d,i)=> acc+d.price ,0);
+   
+    return (
+      <table border = "1"> 
+        <h1 style={{color:"blue"}}> Medicine Data</h1>
         <tr>
-          <th style={{color: "red",padding:"10px"}} >name</th>
-          <th style={{color: "red",padding:"10px"}} >age</th>
-          <th style={{color: "red",padding:"10px"}} >salary</th>
-          <th style={{color: "red",padding:"10px"}} >bouns</th>
-          <th style={{color: "red",padding:"10px"}} >status</th>
-          <th style={{color: "red",padding:"10px"}} > salary bouns</th>
-          <th style={{color: "red",padding:"10px"}} >total bouns</th>
+          <th>name</th>
+          <th>quantity</th>
+          <th>price</th>
+          <th>expiry</th>
+          <th>status</th>
+          <th>total price</th>
         </tr>
-          {
-            filterdata.map ((d,i)=>{
-              return(
-           <>
+        {
+          filterdata.map((d,i)=>{
+            return(
+              <>
                <tr>
                  <td style={{color: "darkblue ",fontweight:"100",padding:"10px"}}>{d.name}</td>
-                 <td style={{color: "darkblue ",fontweight:"100",padding:"10px"}}>{d.age}</td>
-                 <td style={{color: "darkblue ",fontweight:"100",padding:"10px"}}>{d.salary}</td>
-                 <td style={{color: "darkblue ",fontweight:"100",padding:"10px"}}>{d.bonus}</td>
+                 <td style={{color: "darkblue ",fontweight:"100",padding:"10px"}}>{d.quantity}</td>
+                 <td style={{color: "darkblue ",fontweight:"100",padding:"10px"}}>{d.price}</td>
+                 <td style={{color: "darkblue ",fontweight:"100",padding:"10px"}}>{d.expiry}</td>
                  <td style={{color: "darkblue ",fontweight:"100",padding:"10px"}}>{d.status.toString()}</td>
-                 <td style={{color: "darkblue ",fontweight:"100",padding:"10px"}}>{d.salary+d.bonus}</td>
-               {  i === 0 ? <td style={{color: "darkblue ",fontweight:"100",padding:"10px"}} rowSpan={filterdata.length}>{total}</td> : null }
+                 {  i === 0 ? <td style={{color: "darkblue ",fontweight:"100",padding:"10px"}} rowSpan={filterdata.length}>{total}</td> : null }
+      
                  </tr>
            </>
-              )
-            })
-          }
-          
+            )
+          })
+        }
       </table>
-   )
+    )
+
 }
 
 
